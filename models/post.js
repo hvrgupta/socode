7cc40a065f7cfed9ff76ca1625b23a6501e7229d
harshhgupta <harshhgupta16@gmail.com>
2021-03-14 10:23:22 +0530

    Created comment Schema

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'    
-    }
+    },
+    // include the array of ids of all comments in post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 },{
     timestamps: true
 });
