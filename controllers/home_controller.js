af271baf05c4e5add416cac7ce480a2ab8c53e46
harshhgupta <harshhgupta16@gmail.com>
2021-03-15 10:53:24 +0530

    completed database relations (comments,posts)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -16,7 +16,15 @@ module.exports.home = function(req,res) {
     // })
     
     // Populating the user of each post
-    Post.find({}).populate('user').exec(function(err,posts) {
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts) {
         return res.render('home',{
             title: 'Home',
             posts: posts
