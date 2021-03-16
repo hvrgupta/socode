2ecbed725200e68257fab2be6665603b5ab14957
harshhgupta <harshhgupta16@gmail.com>
2021-03-16 11:59:28 +0530

    Added functionality of deleting a Post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req,res) {
     Post.create({
@@ -7,5 +8,21 @@ module.exports.create = function(req,res) {
     },function(err,newPost) {
         if(err) { console.log('Error in creating New Post'); return;}
         return res.redirect('back');    
+    });
+}
+
+module.exports.destroy = function(req,res) {
+    Post.findById(req.params.id,function(err,post) {
+        if(err) { console.log('error in getting post'); return;}
+        // .id means converting the ObjectId into String
+        if(post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id},function(err) {
+                return res.redirect('back');
+            });
+        }else {
+            return res.redirect('back');
+        }
     });
 }
