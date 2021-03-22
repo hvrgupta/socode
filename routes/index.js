4a2990fec078379946a291bec107e2f4d42c6b6a
harshhgupta <harshhgupta16@gmail.com>
2021-03-22 17:02:15 +0530

    set up directory structure for api

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,8 @@ router.use('/posts',require('./posts'));
 
 router.use('/comments',require('./comments'));
 
+router.use('/api',require('./api'));
+
 // for any further routes, access from here
 // router.use('/routerName',require('/routerFile'))
 
