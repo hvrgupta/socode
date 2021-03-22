0a6ef6dd76ad9a84fb3df2d687f5b16d417abe6d
harshhgupta <harshhgupta16@gmail.com>
2021-03-22 17:57:00 +0530

    deleted a post using api link via postman

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,41 @@
-module.exports.index = function(req,res) {
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req,res) {
+
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user','-password')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    });
+
     return res.status(200).json({
         message: 'List of Posts',
-        posts: []
+        posts: posts
     })
+}
+
+module.exports.destroy = async function(req,res) {
+
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // if(post.user == req.user.id) {
+            post.remove();
+            await Comment.deleteMany({post: req.params.id});
+            return res.status(200).json({
+                message: 'Post deleted!'
+            });
+        // }else {
+        //     return res.redirect('back');
+        // }
+    }catch(err) {
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
 }
