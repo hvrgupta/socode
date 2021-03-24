fed96275c21e5160cc0220a8a24f01bbe3ad3d2e
harshhgupta <harshhgupta16@gmail.com>
2021-03-24 19:17:32 +0530

    set up config file for nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'socodedevelopment@gmail.com',
+        pass: 'SocodeHarsh'
+    }
+});
+
+let renderTemplate = (data, relativePath) => {
+    let mailHtml;
+    ejs.renderFile(
+        path.join(__dirname,'../views/mailers',relativePath),
+        data,
+        function(err,template) {
+            if(err) { console.log('error in rendering template'); return;}
+
+            mailHtml = template;    
+        } 
+    )
+    return mailHtml;
+}
+
+module.exports = {
+    transporter,
+    renderTemplate
+}
